2af4dfaaa699d8e5e44db736e022c4a26595999a
chenbitao <cbtpro@gmail.com>
2025-07-13 21:39:04 +0800

    feat: 🎸 子模块

--- src/module.ts
+++ src/module.ts
@@ -1,19 +1,32 @@
-import { defineNuxtModule, addPlugin, createResolver } from '@nuxt/kit'
+import { resolve } from 'node:path';
+import { defineNuxtModule, addPlugin, createResolver } from '@nuxt/kit';
 
 // Module options TypeScript interface definition
-export interface ModuleOptions {}
+export interface ModuleOptions { }
 
+const routes = [
+  {
+    name: 'module-1-home',
+    path: '/module-1-home',
+    file: resolve(__dirname, './runtime/pages/index.vue')
+  },
+];
 export default defineNuxtModule<ModuleOptions>({
   meta: {
-    name: 'my-module',
-    configKey: 'myModule',
+    name: 'my-module-1',
+    configKey: 'myModule1',
   },
   // Default configuration options of the Nuxt module
   defaults: {},
   setup(_options, _nuxt) {
-    const resolver = createResolver(import.meta.url)
+    const resolver = createResolver(import.meta.url);
 
     // Do not add the extension since the `.ts` will be transpiled to `.mjs` after `npm run prepack`
-    addPlugin(resolver.resolve('./runtime/plugin'))
+    addPlugin(resolver.resolve('./runtime/plugin'));
   },
-})
+  hooks: {
+    'pages:extend'(pages) {
+      pages.push(...routes);
+    }
+  }
+});
---
+++ src/runtime/pages/index.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import HelloWorld from '../components/hello-world.vue';
+</script>
+
+<template>
+  <div>my-module-1的首页</div>
+  <HelloWorld />
+</template>
