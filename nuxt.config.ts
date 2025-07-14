d106a49b913524a07ad4eac04e066b721e7951e0
chenbitao <cbtpro@gmail.com>
2025-07-14 20:37:47 +0800

    refactor: 💡 重构代码

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  // 禁用默认的 google 字体提供商
+  fonts: {
+    providers: {
+      google: false,
+      googleicons: false,
+      bunny: false,
+    }
+  },
+  vite: {
+    plugins: []
+  },
   modules: [
     "@nuxt/content",
     "@nuxt/eslint",
-    "@nuxt/fonts",
+    // "@nuxt/fonts",
     "@nuxt/icon",
     "@nuxt/image",
     "@nuxt/scripts",
     "@nuxt/test-utils",
     "@nuxt/ui"
   ]
-})
+});
--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,5 +1,9 @@
 export default defineNuxtConfig({
+  devServer: {
+    host: '0.0.0.0',
+    port: 7750,
+  },
   modules: ['../src/module'],
-  myModule: {},
+  myModule1: {},
   devtools: { enabled: true },
-})
+});
